a265960778aac21aafea36dbcf12bee1a14275ca
Peerapat Wongsuwan <peerapat711@gmail.com>
2023-08-08 18:01:23 +0700

    Update account.service.controller.js

--- controllers/NBAservice/account.service.controller.js
+++ controllers/NBAservice/account.service.controller.js
@@ -47,14 +47,17 @@ module.exports.GetServiceByCateId = async (req, res) => {
 
 module.exports.order = async (req, res) => {
     try {
-        const id = req.body.packageid;
+        const id = req.body.product_detail[0].packageid;
         let data = {
             customer_name: req.body.customer_name?req.body.customer_name:"",
             customer_tel: req.body.customer_tel?req.body.customer_tel:"",
             customer_address: req.body.customer_address?req.body.customer_address:"",
-            shopid: req.user._id,
-            packageid: req.body.packageid,
-            quantity: req.body.quantity
+            product_detail: [{
+                shopid: req.body.product_detail[0].packageid,
+                packageid: req.body.packageid,
+                quantity: req.body.quantity
+            }]
+            
         };
 
         const packageRequestData = {
