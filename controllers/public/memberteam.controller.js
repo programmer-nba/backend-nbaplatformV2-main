008900cda67bd5bf9aea4c22f1e74ee4b8c40666
Peerapat Wongsuwan <peerapat711@gmail.com>
2023-08-10 18:01:05 +0700

    add member getupline controller

---
+++ controllers/public/memberteam.controller.js
@@ -0,0 +1,25 @@
+const { Member } = require("../../models/member.model");
+
+module.exports.getMemberTeam = async (req, res) => {
+    
+    try {
+        const member = await Member.findOne({ tel: req.params.tel })
+        if (!member) {
+            return res.status(403).send({ message: 'เบอร์โทรนี้ยังไม่ได้เป็นสมาชิกของ NBA Platfrom' })
+        } else {
+            const upline = [member.upline.lv1, member.upline.lv2, member.upline.lv3]
+            const validUplines = upline.filter(item => item !== '-')
+            const uplineTel = []
+            for (const item of validUplines) {
+                const include = await Member.findOne({ _id: item })
+                uplineTel.push(include.tel);
+            }
+
+            return res.status(200).send({ message: 'ดึงข้อมูลสำเร็จ', data: uplineTel })
+        }
+    } catch (error) {
+        console.log(error)
+        return res.status(400).send({ message: 'มีบางอย่างผิดพลาด' })
+    }
+
+}
--- routes/public/member.js
+++ routes/public/member.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Member = require('../../controllers/public/member.controller')
+const MemberTeam = require('../../controllers/public/memberteam.controller')
 const auth = require('../../middleware/auth.public');
 
 
@@ -8,5 +9,6 @@ router.get('/tel/:tel', auth, Member.getByTel);
 // router.post('/givecommission', auth, Member.giveCommission);
 // router.post('/givehappypoint', auth, Member.giveHappyPoint);
 // router.post('/transfer_member', Member.transferMember);
+router.get('/memberteam/:tel', auth, MemberTeam.getMemberTeam)
 
 module.exports = router;
