7aa4d535ccad60b8e7013cb64eceac0c415db006
Peerapat Wongsuwan <peerapat711@gmail.com>
2023-08-15 15:08:06 +0700

    update commission method

--- controllers/commission.controller.js
+++ controllers/commission.controller.js
@@ -40,3 +40,24 @@ module.exports.GetUnsummedCommissionsByTel = async (req, res) => {
         return res.status(403).send({ code: error.code, data: error.message });
     };
 }
+
+module.exports.GetUserAllSale = async (req, res) => {
+    try {
+        const tel = req.params.tel
+        const request = {
+            
+            method: 'get',
+            headers: {
+                'auth-token': process.env.SHOP_API_TOKEN
+            },
+            url: `${process.env.SHOP_API}/orderservice/allsale/${tel}`,
+        }
+        await axios(request).then(async (response) => {
+            console.log(response)
+            return res.status(200).send(response.data)
+        })
+    } catch (error) {
+        console.error(error);
+        return res.status(403).send({ code: error.code, data: error.message });
+    };
+}
--- routes/commission.js
+++ routes/commission.js
@@ -4,5 +4,6 @@ const auth  = require('../middleware/auth')
 
 router.get("/totalcommission/:tel", auth, commission.GetCommissionByTel)
 router.get("/list/:tel", auth, commission.GetUnsummedCommissionsByTel)
+router.get("/userallsale/:tel", auth, commission.GetUserAllSale)
 
 module.exports = router
