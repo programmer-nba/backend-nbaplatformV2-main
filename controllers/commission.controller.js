e1569b5c9b0b0a8e0e0334771ffe6b63ff9770bb
Peerapat Wongsuwan <peerapat711@gmail.com>
2023-08-17 13:23:05 +0700

    create get all commission method

--- controllers/commission.controller.js
+++ controllers/commission.controller.js
@@ -81,3 +81,22 @@ module.exports.GetCommissionByOrderId = async (req, res) => {
         return res.status(403).send({ code: error.code, data: error.message });
     };
 }
+
+module.exports.GetAll = async (req, res) => {
+    try {
+        const request = {
+            method: 'get',
+            headers: {
+                'auth-token': process.env.SHOP_API_TOKEN
+            },
+            url: `${process.env.SHOP_API}/commission/list`,
+        }
+        await axios(request).then(async (response) => {
+            console.log(response.data)
+            return res.status(200).send(response.data)
+        })
+    } catch (error) {
+        console.error(error);
+        return res.status(403).send({ code: error.code, data: error.message });
+    };
+}
--- routes/commission.js
+++ routes/commission.js
@@ -3,6 +3,7 @@ const commission = require('../controllers/commission.controller')
 const auth  = require('../middleware/auth')
 
 router.get("/totalcommission/:tel", auth, commission.GetCommissionByTel)
+router.get("/list", auth, commission.GetAll)
 router.get("/list/:tel", auth, commission.GetUnsummedCommissionsByTel)
 router.get("/userallsale/:tel", auth, commission.GetUserAllSale)
 router.get("/listbyorderid/:id", auth, commission.GetCommissionByOrderId)
