4b3ca6ad1208b5ae16e6eb93f3f87b3c868c44da
atthachai <92360791+tutavstuta@users.noreply.github.com>
2023-09-04 11:32:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ app.use('/v2/nba-platform/artwork',require('./routes/artwork/artwork'));
 
 //
 const port = process.env.PORT || 9010;
+
 app.listen(port, ()=>{
     console.log(`API Runing PORT ${port}`);
 });
