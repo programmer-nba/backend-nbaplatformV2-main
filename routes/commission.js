394b41a665b4c19d31f736216286f211e8a39ff9
NBADigital <programmer@nbadigitalworlds.com>
2023-12-18 18:09:35 +0700

    Update commission.js

--- routes/commission.js
+++ routes/commission.js
@@ -1,12 +1,12 @@
-const router = require('express').Router()
-const commission = require('../controllers/commission.controller')
-const auth  = require('../middleware/auth')
+const router = require("express").Router();
+const commission = require("../controllers/commission.controller");
+const auth = require("../middleware/auth");
 
-router.get("/totalcommission", auth, commission.GetCommissionByTel)
-router.get("/list", auth, commission.GetAll)
-router.get("/listbytel", auth, commission.GetUnsummedCommissionsByTel)
-router.get("/userallsale", auth, commission.GetAllSaleByTel)
-router.get("/listbyorderid/:id", auth, commission.GetCommissionByOrderId)
-router.get("/happypoint", auth, commission.GetHappyPointBytel)
+router.get("/totalcommission", auth, commission.GetCommissionByTel);
+router.get("/list", auth, commission.GetAll);
+router.get("/listbytel", auth, commission.GetUnsummedCommissionsByTel);
+router.get("/userallsale", auth, commission.GetAllSaleByTel);
+router.get("/listbyorderid/:id", auth, commission.GetCommissionByOrderId);
+router.get("/happypoint", auth, commission.GetHappyPointBytel);
 
-module.exports = router
+module.exports = router;
