514fdcf38bd01f52c36010a017abefa9227ef058
NBADigital <programmer@nbadigitalworlds.com>
2023-12-28 11:52:37 +0700

    update

--- controllers/NBAservice/graphic.controller.js
+++ controllers/NBAservice/graphic.controller.js
@@ -24,6 +24,28 @@ module.exports.GetService = async (req, res) => {
   }
 };
 
+module.exports.GetCategory = async (req, res) => {
+  try {
+    let token = req.headers["token"];
+    var axios = require("axios");
+    const request = {
+      method: "get",
+      headers: {
+        "auth-token": token,
+        "Content-Type": "application/json",
+      },
+      url: `${process.env.SHOP_API}/artwork/product-graphic/category`,
+    };
+    await axios(request).then(async (response) => {
+      console.log(response.data);
+      return res.status(200).send(response.data.data);
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(403).send({code: error.code, data: error.message});
+  }
+};
+
 module.exports.order = async (req, res) => {
   try {
     const id = req.body.product_detail[0].packageid;
